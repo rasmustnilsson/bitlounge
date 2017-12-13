078c163c910d1f380a2fa800e122185c3966217c
Rasmus Nilsson <zawroli@hotmail.com>
2017-12-13 05:25:24 -0800

    adds dynamic pages for matches

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ module.exports = function(app, passport) {
 
     function isLoggedIn(req, res, next) {
         if (req.isAuthenticated()) return next();
-        res.redirect('/');
+        return res.redirect('/');
     }
     require('./getRoutes')(app,passport,isLoggedIn);
     require('./postRoutes')(app,passport,isLoggedIn);
