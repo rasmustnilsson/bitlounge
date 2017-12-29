46b1c97f520c21becb12bc1f7daf8173d8c5a4b2
Rasmus Nilsson <zawroli@hotmail.com>
2017-12-29 12:45:42 +0100

    now checks for when games are over

--- app/matches.js
+++ app/matches.js
@@ -1,14 +1,37 @@
-const { HLTV } = require('hltv')
-let matches = [];
+const { HLTV } = require('hltv');
+
+let matches = []; // stores all the games, is passed to the browser
+let active = []; // stores only the active games
+
+/**
+ * if match in active is not in HLTV.getMatches response => match is over
+ */
 
 function loadMatches() {
     HLTV.getMatches().then(response => {
-        matches = response;
-    })
+        for(let activeMatch of active) { // loops all the previously active matches
+            activeMatchesIn: for(let i = 0; i < response.length;i++) { // loops and checks if any game is over
+                let match = response[i];
+                // game is not over
+                if(activeMatch.id == match.id) break activeMatchesIn;
+                // game is over
+                if(i == response.length - 1) console.log('game: ' + activeMatch.id + ' is over!');
+            }
+        }
+        // reloads all the matches
+        matches,active = [];
+        for(match in response) {
+            // if game is active
+            if(response[match].live) active.push(response[match]);
+            matches.push(response[match]);
+        }
+    });
 }
-loadMatches();
 
-setInterval(function(){ loadMatches(); }, 1000 * 30);
+loadMatches();
+setInterval(function(){
+    loadMatches();
+}, 1000 * 30);
 
 module.exports = {
     getMatches: function () {
