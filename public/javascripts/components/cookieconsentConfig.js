52027a74e76654ac3fa546d13baf5816daaf6897
Rasmus Nilsson <zawroli@hotmail.com>
2018-01-08 11:17:30 +0100

    adds cookieconsent

---
+++ public/javascripts/components/cookieconsentConfig.js
@@ -0,0 +1,12 @@
+window.cookieconsent.initialise({
+   container: document.getElementById("content"),
+   palette:{
+     popup: {background: "#fff"},
+     button: {background: "#aa0000"},
+   },
+   revokable:true,
+   law: {
+     regionalLaw: false,
+   },
+   location: true,
+ });
